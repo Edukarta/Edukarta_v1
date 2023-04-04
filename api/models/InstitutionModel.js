6c2b38adb0c832c576d196a60e713b7c13cf67e3
Juliadev2 <edukarta.dev2@gmail.com>
2023-04-04 16:12:55 +0200

    model institutions

---
+++ api/models/InstitutionModel.js
@@ -0,0 +1,85 @@
+import mongoose from "mongoose";
+
+const InstitutionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  original_name: {
+    type: String,
+    default: "",
+  },
+  acronym: {
+    type: String,
+    default: "",
+  },
+  imgPath: {
+    type: String,
+    default: "",
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  continent: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  area: {
+    type: String,
+    default: "",
+  },
+  city: {
+    type: String,
+    default: "",
+  },
+  gps: {
+    latitude: {
+      type: String,
+      default: "",
+    },
+    longitude: {
+      type: String,
+      default: "",
+    },
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  foundationDate: {
+    type: String,
+    default: "",
+  },
+  level: {
+    type: String,
+    default: "",
+  },
+  language: {
+    type: String,
+    default: "",
+  },
+  sector: {
+    type: String,
+    default: "",
+  },
+  gender: {
+    type: String,
+    default: "",
+  },
+  religion: {
+    type: String,
+    default: "",
+  },
+  international: {
+    type: String,
+    default: "",
+  },
+});
+
+const Institution = mongoose.model("Institution", InstitutionSchema);
+export default Institution;
