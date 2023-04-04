f8e9dba8dce6e9132116fc44fcdc1741f3daad15
Vince <v.coccina@orange.fr>
2023-04-04 12:07:21 +0200

    setup modif

--- api/server.js
+++ api/server.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 import dbConnect from "./config/dbConnect.js";
 
 dotenv.config();
+//CONFIGURATION
 const app = express();
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send("hello from backend")
