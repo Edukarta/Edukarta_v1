4c659a4c96e4de002ad9d18e1f634b3c9edacae4
Vince <v.coccina@orange.fr>
2023-04-07 17:01:19 +0200

    homepage

--- client/src/schools/pages/HomePage.jsx
+++ client/src/schools/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Map from "../../shared/components/UIElements/Map";
+import CardSugestList from "../components/Homepage/CardSugestList";
 
 const HomePage = () => {
   return (
     <section>
       <Map />
+      <CardSugestList />
     </section>
   );
 };
