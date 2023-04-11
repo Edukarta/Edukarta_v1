cc1e6c5d12efd661cdbcfa096d466a25e45ddbaf
Vince <v.coccina@orange.fr>
2023-04-11 10:25:29 +0200

    login

--- client/src/user/pages/ProfilPage.jsx
+++ client/src/user/pages/ProfilPage.jsx
@@ -1,10 +1,14 @@
 import React, {useState, useEffect} from "react";
 import classes from "./ProfilPage.module.css";
-import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setLogout } from "../../shared/state/store";
+import { useParams, useNavigate } from "react-router-dom";
 
 
 const ProfilPage = () => {
   const [user, setUser] = useState();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { id } = useParams();
 
   const getUser = async () => {
@@ -18,13 +22,17 @@ const ProfilPage = () => {
 
   useEffect(() => {
     getUser();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []);
 
   if (!user) return null;
   return (
     <div className={classes.containerProfil}>
       <div className={classes.container_info}>
         <h1>Hello {user.firstname} {user.lastname}</h1>
+        <button onClick={() => {
+          dispatch(setLogout())
+          navigate("/")
+        }}>Logout</button>
       </div>
     </div>
   );
