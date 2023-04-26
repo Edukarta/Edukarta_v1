4adabd9f21a3928d394b80602888518df35f86d9
Vince <v.coccina@orange.fr>
2023-04-26 18:57:58 +0200

    /

--- api/server.js
+++ api/server.js
@@ -23,9 +23,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
-app.use(cors({
-  origin: "http://165.22.193.67:443" // ou l'URL de votre application front-end
-}));
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
