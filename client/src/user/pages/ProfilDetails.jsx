96c5029c8282c83fff278fa0afcb9a4b9364074d
Vince <v.coccina@orange.fr>
2023-05-18 01:24:39 +0200

    retouche

--- client/src/user/pages/ProfilDetails.jsx
+++ client/src/user/pages/ProfilDetails.jsx
@@ -58,72 +58,76 @@ const ProfilDetails = () => {
 
   return (
     <>
-    <MainNavigation type="profil"/>
-    <div className={classes.containerProfil}>
-      <div className={classes.container_infos}>
-        <h1 className={classes.container_title__profil}>Account Parameters</h1>
-        {previewImage ? (
-          <div className={classes.preview}>
-            <img src={previewImage} alt="" />
-          </div>
-        ) : (
-          <Avatar image={user.imagePath} big />
-        )}
-        <Formik onSubmit={handleFormSubmit} initialValues={initialValuePatch}>
-          {({ values, handleSubmit, setFieldValue, resetForm }) => (
-            <form onSubmit={handleSubmit}>
-              <div>
-                <Dropzone
-                  acceptedFiles=".jpeg,.jpg,.png"
-                  multiple={false}
-                  name="image"
-                  onDrop={(acceptedFiles) =>
-                    handleImageDrop(acceptedFiles, setFieldValue)
-                  }
-                >
-                  {({ getRootProps, getInputProps }) => (
-                    <div {...getRootProps()}>
-                      <input {...getInputProps()} />
-                      {!values.image ? (
-                        <div className={classes.icon__change_pic}>
-                          <Create sx={{ color: "white" }} />
-                        </div>
-                      ) : (
-                        <div className={classes.icon__change_pic_done}>
-                          <Done sx={{ color: "white", fontSize: "40px" }} />
-                        </div>
-                      )}
-                    </div>
-                  )}
-                </Dropzone>
-                {isSubmitting && <LoadingDots/>}
-              </div>
-              <div className={classes.container_profil_details_validate_btn}>
-                {values.image && <Button type="submit">Valider</Button>}
-              </div>
-            </form>
+      <div className={classes.container_navigation}>
+        <MainNavigation type="profil" />
+      </div>
+      <div className={classes.containerProfil}>
+        <div className={classes.container_infos}>
+          <h1 className={classes.container_title__profil}>
+            Account Parameters
+          </h1>
+          {previewImage ? (
+            <div className={classes.preview}>
+              <img src={previewImage} alt="" />
+            </div>
+          ) : (
+            <Avatar image={user.imagePath} big />
           )}
-        </Formik>
-        <div className={classes.container_Input}>
-          <div className={classes.formControl}>
-            <label>Firstname</label>
-            <input type="text" placeholder={user.firstname} disabled />
-          </div>
-          <div className={classes.formControl}>
-            <label>Lastname</label>
-            <input type="text" placeholder={user.lastname} disabled />
-          </div>
-          <div className={classes.formControl}>
-            <label>Address</label>
-            <input type="text" placeholder={user.address} disabled />
-          </div>
-          <div className={classes.formControl}>
-            <label>Phone</label>
-            <input type="text" placeholder={user.phone} disabled />
+          <Formik onSubmit={handleFormSubmit} initialValues={initialValuePatch}>
+            {({ values, handleSubmit, setFieldValue, resetForm }) => (
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <Dropzone
+                    acceptedFiles=".jpeg,.jpg,.png"
+                    multiple={false}
+                    name="image"
+                    onDrop={(acceptedFiles) =>
+                      handleImageDrop(acceptedFiles, setFieldValue)
+                    }
+                  >
+                    {({ getRootProps, getInputProps }) => (
+                      <div {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        {!values.image ? (
+                          <div className={classes.icon__change_pic}>
+                            <Create sx={{ color: "white" }} />
+                          </div>
+                        ) : (
+                          <div className={classes.icon__change_pic_done}>
+                            <Done sx={{ color: "white", fontSize: "40px" }} />
+                          </div>
+                        )}
+                      </div>
+                    )}
+                  </Dropzone>
+                  {isSubmitting && <LoadingDots />}
+                </div>
+                <div className={classes.container_profil_details_validate_btn}>
+                  {values.image && <Button type="submit">Valider</Button>}
+                </div>
+              </form>
+            )}
+          </Formik>
+          <div className={classes.container_Input}>
+            <div className={classes.formControl}>
+              <label>Firstname</label>
+              <input type="text" placeholder={user.firstname} disabled />
+            </div>
+            <div className={classes.formControl}>
+              <label>Lastname</label>
+              <input type="text" placeholder={user.lastname} disabled />
+            </div>
+            <div className={classes.formControl}>
+              <label>Address</label>
+              <input type="text" placeholder={user.address} disabled />
+            </div>
+            <div className={classes.formControl}>
+              <label>Phone</label>
+              <input type="text" placeholder={user.phone} disabled />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
