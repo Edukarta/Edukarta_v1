a98fa9a0982122716527a79ad3a4385caf0afa56
Vince <v.coccina@orange.fr>
2023-06-13 14:19:17 +0200

    dotenv

--- api/server.js
+++ api/server.js
@@ -30,6 +30,7 @@ import nocache from "nocache";
 
 
 //CONGIGURATION
+dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
