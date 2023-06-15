2fb4cbcc617932792a3fdc428d561f12d2cc25bd
dev3-edukarta <edukarta.dev3@gmail.com>
2023-06-15 17:12:48 +0200

    Update Apply.jsx

--- client/src/shared/components/UIElements/Apply.jsx
+++ client/src/shared/components/UIElements/Apply.jsx
@@ -15,7 +15,7 @@ const Apply = (props) => {
 
   const applySchool = async () => {
     const response = await fetch(
-      `http://localhost:5000/api/v1/schools/${props.id}/apply/${user._id}`,
+      `${process.env.REACT_APP_API_URL}/api/v1/schools/${props.id}/apply/${user._id}`,
       {
         method: "PATCH",
         headers: { Authorization: `Bearer ${token}` },
